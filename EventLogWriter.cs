ef75b050094b80746257b60f77c5fb7b6a87f1cb
Adam Presley <adam@adampresley.com>
2012-11-15 19:40:44 -0600

    Initial commit.

---
+++ EventLogWriter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AP_SysLogWriter {
+
+	/**
+	 * Simple class to write events to the Windows Application event log.
+	 * @author Adam Presley
+	 */
+	public class EventLogWriter {
+		public string SourceName { get; set; }
+
+		public EventLogWriter(string sourceName) {
+			this.SourceName = sourceName;
+
+			if (!EventLog.SourceExists(this.SourceName))
+				EventLog.CreateEventSource(this.SourceName, "Application");
+		}
+
+		public void write(string message, EventLogEntryType messageType) {
+			EventLog newLog = new EventLog();
+			newLog.Source = this.SourceName;
+			newLog.WriteEntry(message, messageType);
+		}
+	}
+
+}
